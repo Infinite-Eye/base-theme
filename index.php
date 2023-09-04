c4e84ef9b17a78ccfe2c6ed4c44ad5f38c3810c3
James Collings <james@infinite-eye.com>
2023-09-04 15:49:22 +0100

    Remove test

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 get_header();
 
-echo \InfiniteEye\Media\Media::image(6)
-    ->srcset([
-        // at 1024 display 400px wide image
-        1024 => 400,
-        // at 768 display 200px wide image
-        768 => 200,
-        // at 480 display 100px wide image
-        480 => 100
-    ])
-    ->size(500);
-
 if (have_posts()) {
     while (have_posts()) {
         the_post();
