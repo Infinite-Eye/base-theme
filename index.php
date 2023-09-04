75613acafa1a5b01cb5539c12510a724270b97ff
James Collings <james@infinite-eye.com>
2023-09-04 16:34:43 +0100

    Add basic loop wrapper

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-get_header();
-
-if (have_posts()) {
-    while (have_posts()) {
-        the_post();
-    }
-}
-
-get_footer();
+\InfiniteEye\Theme\Loop::content();
---
+++ template-parts/content.php
@@ -0,0 +1,4 @@
+<div class="<?= post_class(); ?>">
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+</div>
